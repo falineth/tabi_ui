0782317db1a62bf38c4ab370bd1690cb20dbe242
Ben Bassett <185055731+falineth@users.noreply.github.com>
2026-06-09 19:39:23 +1000

    Add web dark mode

---
+++ src/hooks/use_theme_colors/web.rs
@@ -0,0 +1,33 @@
+use dioxus::prelude::*;
+
+const DEFAULT_THEME: &str = include_str!("../../../assets/default_theme.css");
+const DEFAULT_DARK_THEME: &str = include_str!("../../../assets/default_dark_theme.css");
+const DEFAULT_WINDOW_BACKGROUND: (u8, u8, u8, u8) = (0xEF, 0xF0, 0xF1, 0xFF);
+const DEFAULT_DARK_WINDOW_BACKGROUND: (u8, u8, u8, u8) = (0x20, 0x23, 0x26, 0xFF);
+
+pub fn use_theme_colors() -> ReadSignal<String> {
+    use_signal(get_theme_colors).into()
+}
+
+fn get_theme_colors() -> String {
+    if get_is_dark_mode() {
+        DEFAULT_DARK_THEME.to_string()
+    } else {
+        DEFAULT_THEME.to_string()
+    }
+}
+
+pub fn get_window_background_color() -> (u8, u8, u8, u8) {
+    if get_is_dark_mode() {
+        DEFAULT_DARK_WINDOW_BACKGROUND
+    } else {
+        DEFAULT_WINDOW_BACKGROUND
+    }
+}
+
+pub fn get_is_dark_mode() -> bool {
+    web_sys::window()
+        .and_then(|window| window.match_media("(prefers-color-scheme: dark)").ok()?)
+        .map(|query| query.matches())
+        .unwrap()
+}
