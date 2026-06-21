d8a2ef96e48933474d1b845521de6ea0331fa61f
Ben Bassett <185055731+falineth@users.noreply.github.com>
2026-06-21 11:21:27 +1000

    Fix global draw shrinking (#36)

--- src/components/container/mod.rs
+++ src/components/container/mod.rs
@@ -1,7 +1,9 @@
 mod card;
 mod dialog;
+mod global_draw;
 mod tabi_default_context;
 
 pub use card::*;
 pub use dialog::*;
+pub use global_draw::*;
 pub use tabi_default_context::*;
--- src/components/layout/mod.rs
+++ src/components/layout/mod.rs
@@ -1,9 +1,7 @@
-mod global_draw;
 mod portal;
 mod scroll_area;
 mod scroll_host;
 
-pub use global_draw::*;
 pub use portal::*;
 pub use scroll_area::*;
 pub use scroll_host::*;
