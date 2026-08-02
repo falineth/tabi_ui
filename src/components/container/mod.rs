2bfd743ef3a667571ce7fdc0bcd12277fbed90cc
Ben Bassett <185055731+falineth@users.noreply.github.com>
2026-08-02 16:01:56 +1000

    Add form layout components

--- src/components/container/mod.rs
+++ src/components/container/mod.rs
@@ -1,9 +1,11 @@
 mod card;
 mod dialog;
+mod forms;
 mod global_draw;
 mod tabi_default_context;
 
 pub use card::*;
 pub use dialog::*;
+pub use forms::*;
 pub use global_draw::*;
 pub use tabi_default_context::*;
